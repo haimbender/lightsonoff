3419ec7caf9cf6de73162dd925cfae7c327bd4b1
Haim Bender <haimbender@gmail.com>
2017-02-20 19:43:23 +0200

    Making shit better

--- detect.py
+++ detect.py
@@ -5,9 +5,6 @@ import logging.config
 import logging
 import yaml
 
-isHome = None
-outOfHomeCounter = 0
-
 CONFIG = {}
 
 def loadConfigAndLogging():
@@ -20,36 +17,48 @@ def loadConfigAndLogging():
 		logging.config.dictConfig(yaml.load(f.read()))
 
 def outOfDaHouse():
+	logger = logging.getLogger(__name__)
 	logger.info("byebye")
 	for url in CONFIG['OFF_REQUEST_PAGES']:
 		requests.post(url)
 
 def gotBack():
+	logger = logging.getLogger(__name__)
 	logger.info("HELLO")
-	for url in config['ON_REQUEST_PAGES']:
+	for url in CONFIG['ON_REQUEST_PAGES']:
 		requests.post(url)
 
-while True:
+
+def main():
 
 	loadConfigAndLogging()
 
 	logger = logging.getLogger(__name__)
 	logger.info("YO, starting detector")
 
-	result = bluetooth.lookup_name(CONFIG['BLUETOOTH_MAC'], timeout=config['BLUETOOTH_TIMEOUT_SECS'])
-	
-	if result is None:
-		if outOfHomeCounter == config['MAX_COUNTER_FOR_TURN_OFF']:
-			outOfDaHouse()
-			outOfHomeCounter += 1
-		elif outOfHomeCounter < config['MAX_COUNTER_FOR_TURN_OFF']:
-			outOfHomeCounter += 1
-			logger.info("{0}".format(outOfHomeCounter))
-	else:
-		if outOfHomeCounter == config['MAX_COUNTER_FOR_TURN_OFF'] + 1:
+	outOfHomeCounter = 0
+
+	while True:
+
+		result = bluetooth.lookup_name(CONFIG['BLUETOOTH_MAC'], timeout=CONFIG['BLUETOOTH_TIMEOUT_SECS'])
+
+		logger.info("search result:{0}".format(result is not None))
+
+		if result is None:
+			if outOfHomeCounter == CONFIG['MAX_COUNTER_FOR_TURN_OFF']:
+				outOfDaHouse()
+				outOfHomeCounter += 1
+			elif outOfHomeCounter < CONFIG['MAX_COUNTER_FOR_TURN_OFF']:
+				outOfHomeCounter += 1
+				logger.info("{0}".format(outOfHomeCounter))
+		else:
+			if outOfHomeCounter == CONFIG['MAX_COUNTER_FOR_TURN_OFF'] + 1:
+				outOfHomeCounter = 0
+				gotBack()
+
 			outOfHomeCounter = 0
-			gotBack()
-		
-		outOfHomeCounter = 0
-	
-	time.sleep(config['SLEEP_TIME_SECS'])
+
+		time.sleep(CONFIG['SLEEP_TIME_SECS'])
+
+if __name__ == "__main__":
+	main()
